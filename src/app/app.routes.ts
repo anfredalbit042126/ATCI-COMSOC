2d34ebb57bac3929472c89d20345ef6e5c5ea298
anfredalbit042126 <anfredalbit042126@gmail.com>
2026-06-27 14:02:58 +0800

    yes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Project } from './pages/project/project';
+import { Skill } from './pages/skill/skill';
+import { Contact } from './pages/contact/contact';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'about',
+    component: About
+  },
+  {
+    path: 'project',
+    component: Project
+  },
+   {
+    path: 'skill',
+    component: Skill
+  },
+  {
+    path: 'contact',
+    component: Contact
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
